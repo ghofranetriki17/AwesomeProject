f67bdc2764908d0fedde36349fa8cae1a95ddd91
ghofrane <ghoftriki@gmail.com>
2025-12-18 16:31:36 +0100

    fixbug

--- App.tsx
+++ App.tsx
@@ -1,55 +1,29 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useEffect } from 'react';
 import AppTemplate from './src/templates/AppTemplate';
-import ScreenTemplate from './src/templates/ScreenTemplate';
-import Input from './src/components/Input';
-import TouchableButton from './src/components/TouchableButton';
-import LoginPage from './src/screens/Login Page';
 import { NavigationContainer } from '@react-navigation/native';
-import PublicNavigation from './src/navigation/PublicNavigation';
 import AppNavigation from './src/navigation/AppNavigation';
 import { AppProvider } from './src/context/AppContext';
+import BootSplash from 'react-native-bootsplash';
 
 function App() {
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-
-  const handleSubmit = () => {
-    console.log('Email:', email);
-    console.log('Password:', password);
-  };
+  useEffect(() => {
+    // Fallback hide in case NavigationContainer onReady is delayed
+    BootSplash.hide({ fade: true });
+  }, []);
 
   return (
     <AppTemplate>
       <AppProvider>
-        <NavigationContainer>
+        <NavigationContainer
+          onReady={() => {
+            BootSplash.hide({ fade: true });
+          }}
+        >
           <AppNavigation />
         </NavigationContainer>
       </AppProvider>
     </AppTemplate>
-  );//hatina haka khater fl installation kali neshak save area provider
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5f5f5',
-  },
-  form: {
-    width: '80%',
-  },
-  spacer: {
-    height: 16,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginBottom: 20,
-    color: '#333',
-  },
-});
 
 export default App;
--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -1,15 +1,6 @@
-//gerer navigation de lappimport React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
 import PublicNavigation from "./PublicNavigation";
-import BootSplash from "react-native-bootsplash";
-import { useEffect } from "react";
+
 export default function AppNavigation() {
-  useEffect(() => {
-           BootSplash.hide({ fade: true });
-
-
-  }, [])
-  return (
-      <PublicNavigation />
-  );
+  return <PublicNavigation />;
 }
